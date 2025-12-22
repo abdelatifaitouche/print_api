932c90fab52d69bd25f4fd89c79f3637c9c40223
Abdelatif Aitouche <abdelatif.aitouche3@gmail.com>
2025-12-22 17:36:45 +0100

    hotfix(roles) : fixed the roles scope

--- app/endpoints/company_endpoints.py
+++ app/endpoints/company_endpoints.py
@@ -5,8 +5,10 @@ from app.services.company_service import CompanyService
 from sqlalchemy.orm import Session
 from typing import List
 from app.utils.private_route import PrivateRoute
-company_endpoints = APIRouter(        
-        )
+from app.enums.roles import Roles
+from app.enums.permissions import Permissions
+
+company_endpoints = APIRouter()
 
 
 company_service = CompanyService()
@@ -18,14 +20,14 @@ def get_company(company_id : str , db : Session = Depends(get_db) , user : dict 
 
 
 @company_endpoints.post('/' , response_model=CompanyRead)
-def create_company(company_data : CompanyCreate , db:Session=Depends(get_db), user : dict=Depends(PrivateRoute(roles=["admin" , "user"]))): 
+def create_company(company_data : CompanyCreate , db:Session=Depends(get_db), user : dict=Depends(PrivateRoute(roles=[Roles.ADMIN]))): 
     print("user" , user)
     company : CompanyCreate = company_service.create(company_data , db , user["id"])
 
     return company
 
 @company_endpoints.get('/' , response_model=List[CompanyRead])
-def list_companies(db : Session = Depends(get_db) , user : dict = Depends(PrivateRoute(roles=["admin" , "user"])))->List[CompanyRead]:
+def list_companies(db : Session = Depends(get_db) , user : dict = Depends(PrivateRoute(roles=[Roles.ADMIN])))->List[CompanyRead]:
     
     companies : List[CompanyRead] = company_service.list(db , user["id"])
 
@@ -34,15 +36,15 @@ def list_companies(db : Session = Depends(get_db) , user : dict = Depends(Privat
 
 
 
-@company_endpoints.patch('/{company_id}' , response_model=CompanyRead)
+@company_endpoints.patch('/{company_id}/' , response_model=CompanyRead)
 def update_company(company_id : str , company_data : CompanyUpdate , db:Session=Depends(get_db)):
     
     company : CompanyRead = company_service.update(company_id , company_data , db)
 
     return company
 
-@company_endpoints.delete("/{company_id}")
-def delete_company(company_id : str , db:Session=Depends(get_db)):
+@company_endpoints.delete("/{company_id}/")
+def delete_company(company_id : str , db:Session=Depends(get_db) , user : dict = Depends(PrivateRoute(roles=[Roles.ADMIN]))):
 
     if not company_service.delete(company_id , db) : 
         return "not delete"

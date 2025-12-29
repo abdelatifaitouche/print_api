7f869d5b5ea61e7e94d662f28fc4175bf195d6b8
Abdelatif Aitouche <abdelatif.aitouche3@gmail.com>
2025-12-29 20:48:27 +0100

    feat(db) : updated order and order item models

--- app/models/order.py
+++ app/models/order.py
@@ -1,19 +1,17 @@
 from .base import Base
-from sqlalchemy import String , Integer , ForeignKey
+from sqlalchemy import String , Integer , ForeignKey , Sequence , text , Float
 from sqlalchemy.orm import Mapped , mapped_column , relationship
 from app.enums.order_enums import OrderStatus
 
 
 class OrderModel(Base):
     __tablename__="orders"
-
-    order_name:Mapped[str] = mapped_column(String , nullable=False)
+    order_number: Mapped[str] = mapped_column(
+         String(20),
+         server_default=text("concat('ORD#', lpad(nextval('order_number_seq')::text, 5, '0'))")
+    )
     status : Mapped[str] = mapped_column(String , server_default=OrderStatus.PENDING.value)
-    
-    order_price:Mapped[int] = mapped_column(Integer , nullable=False)
-    
-    #add client_id , delivery_date , 
-    
+    order_price:Mapped[Float] = mapped_column(Float , nullable=False) 
     created_by : Mapped[str] = mapped_column(String , ForeignKey("users.id") , nullable=True)
 
     items : Mapped[list["OrderItem"]] = relationship(
--- app/models/order_item.py
+++ app/models/order_item.py
@@ -1,15 +1,22 @@
 from .base import Base
 from sqlalchemy.orm import Mapped , mapped_column , relationship
 from uuid import UUID
-from sqlalchemy import ForeignKey , Integer , String
+from sqlalchemy import ForeignKey , Sequence , String , Float , text , Integer
 from app.enums.order_items_status import OrderItemStatus
 
 class OrderItem(Base):
     __tablename__="order_items"
 
     order_id : Mapped[str]= mapped_column(String , ForeignKey("orders.id") ) 
-    item_name : Mapped[str] = mapped_column(String , nullable=False)
+    product_id : Mapped[str] = mapped_column(String , ForeignKey("products.id"))
+    item_number: Mapped[str] = mapped_column(
+         String(20),
+         server_default=text("concat('item#', lpad(nextval('item_number_seq')::text, 5, '0'))")
+     )
     quantity : Mapped[int] = mapped_column(Integer , nullable=False)
     status : Mapped[str] = mapped_column(String , server_default=OrderItemStatus.PENDING)
-    #add : dims, product_relationship , files_id , price ,   
+    #add : dims, product_relationship , files_id , price ,  
+    item_price : Mapped[Float] = mapped_column(Float , default=0.0 , nullable=True)
+    product : Mapped["ProductModel"] = relationship(back_populates="order_items")
     order : Mapped["OrderModel"] = relationship(back_populates="items")
+    
